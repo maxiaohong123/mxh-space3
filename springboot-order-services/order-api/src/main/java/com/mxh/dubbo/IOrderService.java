115752c99cef1e4a0991c5aa874f161612cf20bf
maxiaohong123 <554349712@qq.com>
2019-08-18 16:58:22 +0800

    'dubbo整合zookeeper'

--- springboot-order-services/order-api/src/main/java/com/mxh/dubbo/IOrderService.java
+++ springboot-order-services/order-api/src/main/java/com/mxh/dubbo/IOrderService.java
@@ -1,6 +1,11 @@
 package com.mxh.dubbo;
 
+import com.mxh.dubbo.dto.Order;
+
 public interface IOrderService {
 
     String sayHello(String name);
+
+    Order addOrder(String orderName);
+
 }
--- springboot-order-services/order-provider/src/main/java/com/mxh/dubbo/impl/OrderServiceImpl.java
+++ springboot-order-services/order-provider/src/main/java/com/mxh/dubbo/impl/OrderServiceImpl.java
@@ -1,6 +1,7 @@
 package com.mxh.dubbo.impl;
 
 import com.mxh.dubbo.IOrderService;
+import com.mxh.dubbo.dto.Order;
 import org.apache.dubbo.config.annotation.Service;
 
 
@@ -8,11 +9,25 @@ import org.apache.dubbo.config.annotation.Service;
 public class OrderServiceImpl implements IOrderService {
     @Override
     public String sayHello(String name) {
-        try {
-            Thread.sleep(1000);
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
+//        try {
+//            Thread.sleep(1000);
+//        } catch (InterruptedException e) {
+//            e.printStackTrace();
+//        }
         return name+":sayHello";
+    }
+
+    @Override
+    public Order addOrder(String orderName) {
+//        try {
+//            Thread.sleep(6000);
+//        } catch (InterruptedException e) {
+//            e.printStackTrace();
+//        }
+       Order order = new Order();
+       order.setId(111);
+       order.setOrderNo(orderName);
+       order.setOrderAddr("中国山西临汾");
+       return order;
     }
 }
