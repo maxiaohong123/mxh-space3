0150e5ff187d801e3281717e54e1fe0fba60e379
maxiaohong123 <554349712@qq.com>
2019-08-18 17:06:10 +0800

    '项目搭建完成，顺利运行'

--- springboot-order-services-web/src/main/java/com/mxh/dubbo/springbootorderservices/IOrderServiceMock.java
+++ springboot-order-services-web/src/main/java/com/mxh/dubbo/springbootorderservices/IOrderServiceMock.java
@@ -1,10 +1,18 @@
 package com.mxh.dubbo.springbootorderservices;
 
 import com.mxh.dubbo.IOrderService;
+import com.mxh.dubbo.dto.Order;
 
 public class IOrderServiceMock implements IOrderService {
     @Override
     public String sayHello(String s) {
         return "调用OrderService异常，返回兜底数据";
+    }
+
+    @Override
+    public Order addOrder(String s) {
+        Order order = new Order();
+        order.setOrderAddr("兜底数据");
+        return  order;
     }
 }
--- springboot-order-services-web/src/main/java/com/mxh/dubbo/springbootorderservices/OrderController.java
+++ springboot-order-services-web/src/main/java/com/mxh/dubbo/springbootorderservices/OrderController.java
@@ -1,6 +1,7 @@
 package com.mxh.dubbo.springbootorderservices;
 
 import com.mxh.dubbo.IOrderService;
+import com.mxh.dubbo.dto.Order;
 import org.apache.dubbo.config.annotation.Reference;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -9,7 +10,8 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class OrderController {
 
-    @Reference(loadbalance = "roundrobin",timeout = 1,cluster ="failfast",check = false,url = "dubbo://localhost:20880/com.mxh.dubbo.IOrderService",mock = "com.mxh.dubbo.springbootorderservices.IOrderServiceMock")  //注意：如果采用的是直连N/A连接服务端，则需要提供url地址。默认采用dubbo协议,协议格式  dubbo://ip:port/接口全路径名
+    @Reference(loadbalance = "roundrobin",timeout = 1,cluster ="failfast",check = false,mock = "com.mxh.dubbo.springbootorderservices.IOrderServiceMock")
+    //注意：如果采用的是直连N/A连接服务端，则需要提供url地址。默认采用dubbo协议,协议格式  dubbo://ip:port/接口全路径名
      IOrderService iOrderService;
 
 
@@ -18,5 +20,12 @@ public class OrderController {
 
         String result = iOrderService.sayHello(name);
         return result;
+    }
+
+    @RequestMapping("test")
+    public String test(@RequestParam(value = "name" ,required = false)String name){
+
+        Order order = iOrderService.addOrder(name);
+        return order.toString();
     }
 }
